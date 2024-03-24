3714195ea5a78fda0a44c7b472d604e74ba49133
r12622036 <r12622036@ntu.edu.tw>
2024-03-24 21:32:54 +0800

    Selecting Language , Changing Function Name

---
+++ a2_q2.py
@@ -0,0 +1,13 @@
+##### Selecting Language , Changing Function Name
+
+This function takes one argument with two non-zero numeric numbers ("x" and "y").
+
+```Python
+def a2_q2(n):
+    answer = {}
+    answer["sum"] = n["x"] + n["y"]
+    answer["difference"] = n["x"] - n["y"]
+    answer["product"] = n["x"] * n["y"]
+    answer["ratio"] = n["x"] / n["y"]
+    return answer
+```
