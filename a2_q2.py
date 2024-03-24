3b6b19e0cc9b30197e1986006855057a692a3827
r12622036 <r12622036@ntu.edu.tw>
2024-03-24 23:24:09 +0800

    Commit for q2-s4

--- a2_q2.py
+++ a2_q2.py
@@ -1,4 +1,4 @@
-##### Adding two new functions
+##### Adding a new function called "xth_alphabet_repeat_y_times"
 
 ##This function takes one argument with two non-zero numeric numbers ("x" and "y").
 ##Python
@@ -9,4 +9,10 @@ def a2_q2(n):
     answer["power_yx"] = pow (n["y"],n["x"])
     answer["repeat_x_times"]= "x"*n["x"]
     answer["repeat_y_times"]=str(n["y"])*n["y"]
+    for key, value in (n):
+        if key == "x":
+            xth_alphabet = chr(ord('a')+value-1)
+        elif key == "y":
+            result = xth_alphabet * value
+    answer["xth_alphabet_repeat_y_times"]= result
     return answer
