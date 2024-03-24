fc55064430b31b081153890b6c3a0055f6527373
r12622036 <r12622036@ntu.edu.tw>
2024-03-24 23:11:13 +0800

    Commit for q2-s3

--- a2_q2.py
+++ a2_q2.py
@@ -1,4 +1,4 @@
-##### Correcting the function 
+##### Adding two new functions
 
 ##This function takes one argument with two non-zero numeric numbers ("x" and "y").
 ##Python
@@ -7,4 +7,6 @@ def a2_q2(n):
     answer = {}
     answer["power_xy"] = pow (n["x"],n["y"]) 
     answer["power_yx"] = pow (n["y"],n["x"])
+    answer["repeat_x_times"]= "x"*n["x"]
+    answer["repeat_y_times"]=str(n["y"])*n["y"]
     return answer
