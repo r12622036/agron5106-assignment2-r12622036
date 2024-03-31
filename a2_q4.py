c49b861dfad8bf47c5a16e8ae74a3c44d8525bcd
r12622036 <r12622036@ntu.edu.tw>
2024-03-31 17:00:46 +0800

    Initial part of a2_q4
    Writing code for the 'diff_num' function, which splits a group into two separate groups, each containing half of the elements.

---
+++ a2_q4.py
@@ -0,0 +1,10 @@
+#This function will divide the input into two groups of equal sizes, where the difference between the sums of each group is minimised
+def diff_num(x:list):
+    group_1=[]
+    group_2=[]
+    length_x = len(x)
+    for i in range(length_x//2):
+        group_1.append(x[i])
+    for i in range(length_x//2,length_x):
+        group_2.append(x[i])
+        
