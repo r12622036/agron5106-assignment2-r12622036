793ad4ead4a11e90278770d6131c362325e8b009
r12622036 <r12622036@ntu.edu.tw>
2024-03-31 17:11:25 +0800

    Adding return for the function
    As previous function didn't have any return, might troublesome for checking the function

--- a2_q4.py
+++ a2_q4.py
@@ -1,5 +1,6 @@
 #This function will divide the input into two groups of equal sizes, where the difference between the sums of each group is minimised
 #Adding the difference equation into the diff_num function 
+#Adding the return as the previous function didn't have any return 
 def diff_num(x:list):
     group_1=[]
     group_2=[]
@@ -8,4 +9,5 @@ def diff_num(x:list):
         group_1.append(x[i])
     for i in range(length_x//2,length_x):
         group_2.append(x[i])
-    diff= abs(sum(group_1)-sum(group_2))    
+    diff= abs(sum(group_1)-sum(group_2))
+    return group_1,group_2,diff 
